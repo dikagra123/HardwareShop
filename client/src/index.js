b3ed26f1d90fb97ec5ed26f45b7afb2c63059515
Diksha Agrawal <dikagra123@email.com>
2026-05-17 00:52:36 +0530

    fix CORS for Netlify

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-const API = axios.create({
-  baseURL: 'https://hardware-shop1.onrender.com',
-});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
